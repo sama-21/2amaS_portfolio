f3e37d0a5a4528d718283bb0ead56381db8b3b94
sama-21 <wasamatsameer@gmail.com>
2022-03-31 10:53:04 +0530

    personal

---
+++ code/app.js
@@ -0,0 +1,61 @@
+// JavaScript
+// Button Subscribe
+const buttonSubs = document.querySelector("#btn-subscribe");
+
+buttonSubs.addEventListener('click',() => {
+    buttonSubs.innerHTML = "Thank you !!!"
+});
+
+// jQuery
+
+// Circle
+$(document).ready(function () {
+    $("#card1").hover(function () {
+        $("#card-img-overlay1").toggle(100, "swing")
+    })
+})
+
+$(document).ready(function () {
+    $("#card2").hover(function () {
+        $("#card-img-overlay2").toggle(100, "swing")
+    })
+})
+
+// FlexStart
+$(document).ready(function () {
+    $("#card3").hover(function () {
+        $("#card-img-overlay3").toggle(100, "swing")
+    })
+})
+
+$(document).ready(function () {
+    $("#card4").hover(function () {
+        $("#card-img-overlay4").toggle(100, "swing")
+    })
+})
+
+// Printed
+$(document).ready(function () {
+    $("#card5").hover(function () {
+        $("#card-img-overlay5").toggle(100, "swing")
+    })
+})
+
+$(document).ready(function () {
+    $("#card6").hover(function () {
+        $("#card-img-overlay6").toggle(100, "swing")
+    })
+})
+
+// Silicon
+$(document).ready(function () {
+    $("#card7").hover(function () {
+        $("#card-img-overlay7").toggle(100, "swing")
+    })
+})
+
+$(document).ready(function () {
+    $("#card8").hover(function () {
+        $("#card-img-overlay8").toggle(100, "swing")
+    })
+})
